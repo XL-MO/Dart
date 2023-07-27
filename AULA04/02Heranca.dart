d4d5e4dbf25ab83556622336bf837981305668b6
Gisely Oliveira <giselymoli@gmail.com>
2023-07-27 10:03:46 -0300

    AUla de dart

---
+++ AULA04/02Heranca.dart
@@ -0,0 +1,17 @@
+import 'dart:web_gl';
+
+class Pessoa extends Endereco{
+  String? nome;
+  int? idade;
+}
+
+class Endereco{
+  String? bairro;
+  String? logradouro;
+    int? numero;
+}
+
+Run|Debug
+void main(){
+  
+}
